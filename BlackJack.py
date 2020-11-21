17e70957ba100783e7af8f1c55777108e80ffb65
Eric Mata <ericmata.93@outlook.com>
2020-11-20 20:04:51 -0500

    Create BlackJack.py
    Description of what blackjack is

---
+++ BlackJack.py
@@ -0,0 +1,9 @@
+#This is Eric's Blackjack Python casion game 
+
+""" 
+The goal of the game is to get as close to 21 as possible without going over.
+It is players (up to 4) vs dealer. The players are dealt two cards face up,
+and the dealer gets 1 card face up and 1 face down. Each player chooses "hit"
+or "stay" to get another card or stop receiving car
+
+"""
